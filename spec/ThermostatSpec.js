c376549d4d6ac572462d84be78f3ffac52dec962
jinimcoroneo <jinim1983@gmail.com>
2017-07-05 16:31:10 +0100

    add min temperature and power saving mode function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,24 +3,43 @@ describe("Thermostat", function() {
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-  
+
   it("defaults to 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
   });
-  
+
+  it("has a minimum temperature of 10 degrees", function() {
+    expect(thermostat.min).toEqual(10);
+  });
+
+  it("defaults with the power saving mode on", function() {
+    expect(thermostat.powerSavingModeOn).toEqual(true);
+  });
+
   describe("has an up function that", function() {
     it("can increase the temperature", function() {
       thermostat.up(2);
       expect(thermostat.temperature).toEqual(defaultTemperature + 2);
     });
   });
-  
+
   describe("has a down function that", function() {
     it("can decrease the temperature", function() {
       thermostat.down(2);
       expect(thermostat.temperature).toEqual(defaultTemperature - 2);
     });
+
+    it("cannot go below the minimum temperature", function() {
+      expect(function() {
+        thermostat.down(11);
+      }).toThrow(new ThermostatException("Can't go below the " + thermostat.min + "temperature"));
+    })
   });
-  
-  
+
+  describe("has a power saving mode function that", function() {
+    it("when on, changes the max temperature to 25 degrees", function() {
+      thermostat.switchPowerSavingMode();
+      expect(thermostat.powerSavingModeOn).toEqual(false);
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,14 +1,31 @@
 var defaultTemperature = 20;
+var minTemperature = 10;
+var maxTemperatureModeOn = 25;
+
+function ThermostatException(message) {
+  this.name = "ThermostatException";
+  this.message = message;
+}
 
 function Thermostat() {
   this.temperature = defaultTemperature;
+  this.min = minTemperature;
+  this.powerSavingModeOn = true;
 };
 
-Thermostat.prototype.up = function (number) {
+Thermostat.prototype.up = function(number) {
   return (this.temperature += number);
 };
 
 
-Thermostat.prototype.down = function (number) {
+Thermostat.prototype.down = function(number) {
+  if(this.temperature - number < this.min) {
+    throw new ThermostatException("Can't go below the " + this.min + "temperature")
+  }
   return (this.temperature -= number);
+};
+
+
+Thermostat.prototype.switchPowerSavingMode = function() {
+  return this.powerSavingModeOn = !this.powerSavingModeOn
 };
